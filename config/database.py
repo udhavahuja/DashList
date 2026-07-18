b641013d64209f6cf2e2bc73365e97d5446dafb4
Udhav Ahuja <udhav19ahuja@gmail.com>
2026-07-18 19:51:34 +0530

    Fixed SQLite instance directory creation

--- config/database.py
+++ config/database.py
@@ -1,6 +1,8 @@
+import os
 import sqlite3
 from .config import DATABASE_PATH
 
+os.makedirs("instance", exist_ok=True)
 def get_db():
     conn = sqlite3.connect(DATABASE_PATH)
     conn.row_factory = sqlite3.Row
